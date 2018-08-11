aa4af5332d958c8af1c3e32e9d27803272ee90c7
github username <github email address>
2018-08-11 17:57:13 -0400

    removed testing code from reducer. Redux still working as expected.

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,16 +1,5 @@
 const defaultState = {
-  artworkArray: [{
-_id: "5b6bbda3e93e040014c68839",
-title: "Wanderer Above the Sea of Fog",
-artist: "Caspar David Friedrich",
-medium: "Oil on canvas",
-century: "19th Century",
-culture: "German",
-url: "https://en.wikipedia.org/wiki/Wanderer_above_the_Sea_of_Fog",
-imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Caspar_David_Friedrich_-_Wanderer_above_the_sea_of_fog.jpg",
-apiId: 315,
-__v: 0
-}],
+  artworkArray: [],
   // testString: "this is a test string defined in the default state."
 }
 
