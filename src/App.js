c78a15b2cff1cbd61cbfbecfcb162b24a0abb1b1
github username <github email address>
2018-08-12 13:29:15 -0400

    fixed broken view routing with withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
 import NavBar from "./components/NavBar"
 import IndexContainer from "./components/containers/IndexContainer";
-import DetailContainer from "./components/containers/DetailContainer"
+import DetailContainer from "./components/containers/DetailContainer";
+
 
 // import { connect } from 'react-redux'
 // import { changeExampleMessage } from './actions'
@@ -30,4 +32,11 @@ class App extends Component {
   }
 }
 
-export default withRouter(App);
+function mapStateToProps(state){
+  return {
+    artworkArray: state.artworkArray,
+    selectedArtwork: state.selectedArtwork,
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App));
