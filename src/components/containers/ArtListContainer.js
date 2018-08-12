77e2a9cc8fb58bff8f5c2302d3fcd2342724cf02
github username <github email address>
2018-08-11 20:09:02 -0400

    tidied code. redux still working

--- src/components/containers/ArtListContainer.js
+++ src/components/containers/ArtListContainer.js
@@ -3,40 +3,6 @@ import { connect } from 'react-redux';
 import ArtCard from "../ArtCard"
 
 class ArtListContainer extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      tempArray: [{
-        title: "Guernica",
-        artist: "Pablo Picasso",
-        medium: "Oil on canvas",
-        century: "20th Cenutry",
-        culture: "Spanish",
-        url: "https://en.wikipedia.org/wiki/Guernica_(Picasso)",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/74/PicassoGuernica.jpg",
-        apiId: 568,
-      },{
-        title: "Las Meninas",
-        artist: "Diego Velazquez",
-        medium: "Oil on canvas",
-        century: "19th Cenutry",
-        culture: "Spanish",
-        url: "https://en.wikipedia.org/wiki/Las_Meninas",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Las_Meninas%2C_by_Diego_Vel%C3%A1zquez%2C_from_Prado_in_Google_Earth.jpg/800px-Las_Meninas%2C_by_Diego_Vel%C3%A1zquez%2C_from_Prado_in_Google_Earth.jpg",
-        apiId: 754,
-      },{
-        title: "Nighthawks",
-        artist: "Edward Hopper",
-        medium: "Oil on canvas",
-        century: "20th Cenutry",
-        culture: "American",
-        url: "https://en.wikipedia.org/wiki/Nighthawks",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Nighthawks_by_Edward_Hopper_1942.jpg",
-        apiId: 145,
-      }],
-    }
-  }
 
   render(){
     return(
