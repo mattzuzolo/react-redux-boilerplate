5a21e453c38eaeaa8bffd2118319f057510bc747
github username <github email address>
2018-08-12 14:08:23 -0400

    cleaned up code. Still working. Next step is implementing dynamic routes

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -24,8 +24,8 @@ export default function(state = defaultState, action) {
     case "CHANGE_MESSAGE":
       return {testString: action.payload}
 
-      case "SELECT_ARTWORK":
-        return {...state, selectedArtwork: action.payload}
+    case "SELECT_ARTWORK":
+      return {...state, selectedArtwork: action.payload}
 
     default:
       return state
